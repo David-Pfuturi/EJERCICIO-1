d2015dbaa1b78d1623b457558c561d28c5ce7f0b
David-Pfuturi <38045615+David-Pfuturi@users.noreply.github.com>
2018-04-12 22:53:48 -0500

    prob-4

--- problem 4.cpp
+++ problem 4.cpp
@@ -1,10 +1,10 @@
-void prod_matriz(int M1[][10],int M2[][10],int f,int c)//matrices cuadradas...
+void prod_matriz(int M1[][10],int M2[][10],int n)//matrices cuadradas...
 {
     int matriz3[10][10];
-    for (int i=0;i<f;i++){
-        for (int j=0;j<c;j++){
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
             matriz3[i][j]=0; 
-            for (int k=0;k<f;k++){
+            for (int k=0;k<n;k++){
                 matriz3[i][j] += M1[i][k]*M2[k][j];
             }
         }
