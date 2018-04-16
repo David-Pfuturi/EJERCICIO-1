b62e0b3bb81e254e3351a0d85954c184bb34e0a4
David-Pfuturi <38045615+David-Pfuturi@users.noreply.github.com>
2018-04-15 23:11:49 -0500

    prob 9

---
+++ problem 9.cpp
@@ -0,0 +1,20 @@
+bool palindromo(char *c)
+{
+    int longit=0;
+    while(*(c++)){
+        longit++;
+    }
+    cout<<longit<<endl;
+    for(int i=0;i<longit/2;i++){
+        if (*(c+i)!=*(c+longit-i-1)){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    char pal[]="7337";
+    cout<<palindromo(pal);
+}
